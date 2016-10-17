351f058fe270660956bf7dc317cf2796c6e652ca
OptimizLabs <rachidonline@gmail.com>
2016-10-17 09:25:41 +0100

    Update JSCarousel.js

--- JSCarousel.js
+++ JSCarousel.js
@@ -1,9 +1,11 @@
 /*
  * JSCarousel
- * version: 1.0.0
+ * Version: 1.0.0
  * Author: Rachid HARAKAT
  * Author mail: rachidonline<at>gmail<dot>com
- */
+ * Project link: https://optimizlab.github.io/JSCarousel/
+ **/
+
 
 (function () {
 
@@ -21,37 +23,36 @@
             this.btnPrev = this.navButtons.querySelector(args.btnPrev) || null;
         }
 
+        this.autoPlay = parseFloat(args.autoPlay) || 0;
+
         this.carouselWidth = 0;
-        this.activeSlideId = 0;
+        this.activeSlideId = 1;
         this.anchorButtons = [];
 
-        /*
-	 console.log(this.carousel);
-         console.log(this.slides);
-         console.log(this.btnIndex);
-         console.log(this.btnNext);
-         console.log(this.btnPrev);
-	 */
         this.init();
     }
-    ;
 
     JSCarousel.prototype.init = function () {
         /*
          * First our slider is linear and horizontal
          * Make slide container fite to slides with			
          **/
+
+//        console.log(this);
+
         this.setContainerWidth();
-	    
+
         /*
          * Add buttons indexs
          **/
         if (this.navButtons) {
             this.setAnchorsButton();
         }
+        if (this.autoPlay > 0) {
+            this.setAutoPlay();
+        }
 
     };
-
     JSCarousel.prototype.setContainerWidth = function () {
         var i = 0;
         for (i; i < this.slides.length; i++) {
@@ -65,25 +66,23 @@
         }
         this.carousel.style.width = this.carouselWidth + 'px';
     };
-
     JSCarousel.prototype.setAnchorsButton = function () {
         /*
          * Geven container as a ul
-         * creating li>button (<button data-index="7" type="button"></button>)
+         * Creating li > button (<button data-index="7" type="button"></button>)
          **/
         for (var i = 1; i <= this.slides.length; i++) {
-		
+
             var mLi = document.createElement('li');
             var mBut = document.createElement('button');
             mBut.innerHTML = i;
             mBut.setAttribute("data-index", i);
             mBut.setAttribute("type", 'button');
             mBut.setAttribute('class', 'anchor');
-		
+
             if (i === 1) {
                 mLi.setAttribute('class', 'first');
-            }
-            ;
+            }            
 
             addGoToEventListener(mBut, this);
 
@@ -95,20 +94,20 @@
         addGoToNextEventListener(this.btnNext, this);
         addGoToPreviewEventListener(this.btnPrev, this);
     };
-
+    JSCarousel.prototype.setAutoPlay = function () {
+        var _ = this;
+        setInterval(function () {
+            _.goToNext();
+        }, this.autoPlay * 1000);
+    };
     JSCarousel.prototype.goTo = function (pos) {
         var pos = parseInt(pos);
-
-        console.log('Go to pos: ' + pos);
-
         this.activeSlideId = pos - 1;
-
         if (pos === 1) {
             this.carousel.style.left = '0px';
         } else {
             this.carousel.style.left = parseFloat(((this.carouselWidth / this.slides.length) * (pos - 1)) * -1) + 'px';
         }
-
         //change visibility
         for (var i = 0; i < this.slides.length; i++) {
             this.changeOpacityAndVisibility(i);
@@ -117,7 +116,7 @@
     JSCarousel.prototype.goToNext = function () {
         var pos = this.activeSlideId + 1;
 
-        console.log('goToNext: ' + pos);
+//        console.log('goToNext: ' + pos);
 
         if (this.activeSlideId >= this.slides.length - 1) {
             pos = 1;
@@ -131,7 +130,7 @@
     JSCarousel.prototype.goToPreview = function () {
         var pos = this.activeSlideId;
 
-        console.log('goToPreview: ' + pos);
+//        console.log('goToPreview: ' + pos);
 
         if (pos <= 0) {
             pos = this.slides.length;
@@ -142,11 +141,27 @@
         this.activeSlideId = pos;
         this.goTo(pos);
     };
+    JSCarousel.prototype.changeOpacityAndVisibility = function (index) {
+        if (index === this.activeSlideId) {
+            this.slides[index].setAttribute('data-active', true);
+            if (this.navButtons) {
+                //Active anchor button
+                this.anchorButtons[this.activeSlideId].className += ' active';
+            }
+        } else {
+            this.slides[index].setAttribute('data-active', false);
+            if (this.navButtons) {
+                //Disactive anchor button
+                this.anchorButtons[index].setAttribute('class', 'anchor');
+            }
+        }
+    };
+
     /*
-     *	a=>elem;
-     *   b=> context
+     *	a => Elem;
+     *  b => Context
      */
-    function addGoToEventListener(a, b) 
+    function addGoToEventListener(a, b) {
         if (window.addEventListener) {
             a.addEventListener('click', function () {
                 b.goTo(parseInt(this.getAttribute('data-index')));
@@ -161,7 +176,6 @@
             };
         }
     }
-
     function addGoToNextEventListener(a, b) {
         if (window.addEventListener) {
             a.addEventListener('click', function () {
@@ -192,24 +206,6 @@
             };
         }
     }
-
-    JSCarousel.prototype.changeOpacityAndVisibility = function (index) {
-        if (index === this.activeSlideId) {
-            this.slides[index].setAttribute('data-active', true);
-            if (this.navButtons) {
-                //Active anchor butt
-                this.anchorButtons[this.activeSlideId].className += ' active';
-            }
-
-        } else {
-            this.slides[index].setAttribute('data-active', false);
-            if (this.navButtons) {
-                //Disactive anchor button
-                this.anchorButtons[index].setAttribute('class', 'anchor');
-            }
-        }
-    };
-
 
     window.JSCarousel = JSCarousel || {};
 })();
